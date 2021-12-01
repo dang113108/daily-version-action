dd577615e8e3ef260790ead07894f06aae1cf612
AndrewChen <chenpoanandrew@gmail.com>
2021-12-01 23:34:32 +0800

    fix: run build.

--- distribution/index.js
+++ distribution/index.js
@@ -69,79 +69,79 @@ module.exports = require("os");
 const core = __webpack_require__(470);
 const util = __webpack_require__(669);
 const dayjs = __webpack_require__(629);
-var utc = __webpack_require__(130);
-var timezone = __webpack_require__(93);
+const utc = __webpack_require__(130);
+const timezone = __webpack_require__(93);
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
 const execFile = util.promisify(__webpack_require__(129).execFile);
 
 async function init() {
-  // Use ENV if it's a `push.tag` event
-  if (process.env.GITHUB_REF.startsWith("refs/tags/")) {
-    const pushedTag = process.env.GITHUB_REF.replace("refs/tags/", "");
-    core.setOutput("version", process.env.GITHUB_REF.replace("refs/tags/", ""));
-    core.info(
-      "Run triggered by tag `" +
+	// Use ENV if it's a `push.tag` event
+	if (process.env.GITHUB_REF.startsWith('refs/tags/')) {
+		const pushedTag = process.env.GITHUB_REF.replace('refs/tags/', '');
+		core.setOutput('version', process.env.GITHUB_REF.replace('refs/tags/', ''));
+		core.info(
+			'Run triggered by tag `' +
         pushedTag +
-        "`. No new tags will be created by `daily-version-action`."
-    );
-    return;
-  }
+        '`. No new tags will be created by `daily-version-action`.'
+		);
+		return;
+	}
 
-  // Look for tags on the current commit
-  await execFile("git", [
-    "fetch",
-    "--depth=1",
-    "origin",
-    "refs/tags/*:refs/tags/*",
-  ]);
-  const { stdout: tagsOnHead } = await execFile("git", [
-    "tag",
-    "-l",
-    "--points-at",
-    "HEAD",
-  ]);
-  if (tagsOnHead) {
-    const [mostRecentTag] = tagsOnHead.split("\n"); // `stdout` may contain multiple tags
-    core.setOutput("version", mostRecentTag);
-    core.info(
-      "No new commits since the last tag (" +
+	// Look for tags on the current commit
+	await execFile('git', [
+		'fetch',
+		'--depth=1',
+		'origin',
+		'refs/tags/*:refs/tags/*'
+	]);
+	const {stdout: tagsOnHead} = await execFile('git', [
+		'tag',
+		'-l',
+		'--points-at',
+		'HEAD'
+	]);
+	if (tagsOnHead) {
+		const [mostRecentTag] = tagsOnHead.split('\n'); // `stdout` may contain multiple tags
+		core.setOutput('version', mostRecentTag);
+		core.info(
+			'No new commits since the last tag (' +
         mostRecentTag +
-        "). No new tags will be created by `daily-version-action`."
-    );
-    return;
-  }
+        '). No new tags will be created by `daily-version-action`.'
+		);
+		return;
+	}
 
-  // A new tag must be created
-  const version = dayjs().tz("Asia/Taipei").format("YYYY.MM.DD.HHmm");
-  core.info(
-    "HEAD isn’t tagged. `daily-version-action` will create `" + version + "`"
-  );
+	// A new tag must be created
+	const version = dayjs().tz('Asia/Taipei').format('YYYY.MM.DD.HHmm');
+	core.info(
+		'HEAD isn’t tagged. `daily-version-action` will create `' + version + '`'
+	);
 
-  core.setOutput("version", version);
+	core.setOutput('version', version);
 
-  // Ensure that the git user is set
-  const hasEmail = await execFile("git", ["config", "user.email"]).catch(
-    (_) => false
-  );
-  if (!hasEmail) {
-    await execFile("git", [
-      "config",
-      "user.email",
-      "actions@users.noreply.github.com",
-    ]);
-    await execFile("git", ["config", "user.name", "daily-version-action"]);
-  }
+	// Ensure that the git user is set
+	const hasEmail = await execFile('git', ['config', 'user.email']).catch(
+		_ => false
+	);
+	if (!hasEmail) {
+		await execFile('git', [
+			'config',
+			'user.email',
+			'actions@users.noreply.github.com'
+		]);
+		await execFile('git', ['config', 'user.name', 'daily-version-action']);
+	}
 
-  // Create tag and push it
-  await execFile("git", ["tag", version, "-m", version]);
-  await execFile("git", ["push", "origin", version]);
-  core.setOutput("created", "yes");
+	// Create tag and push it
+	await execFile('git', ['tag', version, '-m', version]);
+	await execFile('git', ['push', 'origin', version]);
+	core.setOutput('created', 'yes');
 }
 
-init().catch((error) => {
-  core.setFailed(error.name + " " + error.message);
+init().catch(error => {
+	core.setFailed(error.name + ' ' + error.message);
 });
 
 
